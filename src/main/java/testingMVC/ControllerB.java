1ab5949d4f77b9a6bb333d3f4a200fe45f12bb42
Dave Nelson <savenelson@gmail.com>
2018-02-20 18:29:45 -0500

    second commit

---
+++ src/main/java/testingMVC/ControllerB.java
@@ -0,0 +1,20 @@
+package testingMVC;
+
+import javafx.stage.Stage;
+
+public class ControllerB {
+    private final Stage primaryStage;
+    private final ViewB view = new ViewB(this);
+
+    public ControllerB(final Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public ViewB getView() {
+        return view;
+    }
+}
